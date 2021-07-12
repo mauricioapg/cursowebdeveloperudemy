d285aa1c15a4886cc9bb945ee639210d528fdc66
Mauricio Aparecido Gabriel <mauricio.gabriel@newceb.com.br>
2021-07-12 20:12:53 -0300

    finalizando projeto galeria (boostrap, jquery, webpack)

---
+++ bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,10 @@
+//ARQUIVOS SASS
+import './scss/index.scss'
+
+//Dependências
+import 'jquery'
+import 'bootstrap'
+
+//Meus Arquivos JS
+import './js/core/includes'
+import './js/plugins/cityButtons'
